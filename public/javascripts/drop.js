a6244778132b559ab5bec6ff8794350f78189a0c
Zuza <zuzannazborowska@gmail.com>
2017-11-15 10:30:33 +0100

    preview of the picture

--- public/javascripts/drop.js
+++ public/javascripts/drop.js
@@ -1,41 +1,76 @@
-
-/*$(document).ready(function()
-{
-    $("#drop-area").on('dragenter', function (e){
-        e.preventDefault();
-        $(this).css('background', '#BBD5B8');
-    });
-
-    $("#drop-area").on('dragover', function (e){
-        e.preventDefault();
-    });
-
-    $("#drop-area").on('drop', function (e){
-        $(this).css('background', '#D8F9D3');
-        e.preventDefault();
-        var image = e.originalEvent.dataTransfer.files;
-        var fileReader = new FileReader();
-    });
-});*/
-
-function createFormData(image)
-{
-    var formImage = new FormData();
-    formImage.append('userImage', image[0]);
-    uploadFormData(formImage);
-}
+/*
 
 function uploadFormData(formData)
-{
-    /*$.ajax({
-        url: "upload_image.php",
-        type: "POST",
-        data: formData,
-        contentType:false,
-        cache: false,
-        processData: false,
-        success: function(data){
-            $('#drop-area').html(data);
-        }});*/
+if (window.FileReader) {
+    var drop;
+    addEventHandler(window, 'load', function() {
+        var status = document.getElementById('status');
+        drop = document.getElementById('drop');
+        var list = document.getElementById('list');
 
+
+        // Tells the browser that we *can* drop on this target
+        addEventHandler(drop, 'dragover', cancel);
+        addEventHandler(drop, 'dragenter', cancel);
+
+        addEventHandler(drop, 'drop', function(e) {
+            e = e || window.event; // get window.event if e argument missing (in IE)
+            if (e.preventDefault) {
+                e.preventDefault();
+            } // stops the browser from redirecting off to the image.
+
+            var dt = e.dataTransfer;
+            var files = dt.files;
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                var reader = new FileReader();
+
+                //attach event handlers here...
+
+                reader.readAsDataURL(file);
+                addEventHandler(reader, 'loadend', function(e, file) {
+                    var bin = this.result;
+                    var newFile = document.createElement('div');
+                    newFile.innerHTML = 'Loaded : ' + file.name + ' size ' + file.size + ' B';
+                    list.appendChild(newFile);
+                    var fileNumber = list.getElementsByTagName('div').length;
+                    status.innerHTML = fileNumber < files.length ?
+                        'Loaded 100% of file ' + fileNumber + ' of ' + files.length + '...' :
+                        'Done loading. processed ' + fileNumber + ' files.';
+
+                    var img = document.createElement("img");
+                    img.file = file;
+                    img.src = bin;
+                    list.appendChild(img);
+                }.bindToEventHandler(file));
+            }
+            return false;
+        });
+        Function.prototype.bindToEventHandler = function bindToEventHandler() {
+            var handler = this;
+            var boundParameters = Array.prototype.slice.call(arguments);
+            console.log(boundParameters);
+            //create closure
+            return function(e) {
+                e = e || window.event; // get window.event if e argument missing (in IE)
+                boundParameters.unshift(e);
+                handler.apply(this, boundParameters);
+            }
+        };
+    });
+} else {
+    document.getElementById('status').innerHTML = 'Your browser does not support the HTML5 FileReader.';
 }
+
+function addEventHandler(obj, evt, handler) {
+    if (obj.addEventListener) {
+        // W3C method
+        obj.addEventListener(evt, handler, false);
+    } else if (obj.attachEvent) {
+        // IE method.
+        obj.attachEvent('on' + evt, handler);
+    } else {
+        // Old school method.
+        obj['on' + evt] = handler;
+    }
+}*/
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,11 @@
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    // Great success! All the File APIs are supported.
+} else {
+    alert('The File APIs are not fully supported in this browser.');
+}
+$("document").on('drop',function(event){
+    event.preventDefault();
+});
 
 $("document").ready(function(){
 
@@ -14,17 +22,42 @@ function readURL(input) {
 
         var reader = new FileReader();
 
-
         reader.onload = function(e) {
 
             $('#userImage').attr('src', e.target.result);
-
         }
-
        reader.readAsDataURL(input.files[0]);
-        console.log(reader.result);
-
     }
 }
+function dragStart(event) {
 
+    event.dataTransfer.setData("img", event.target.id);
+}
 
+function handleDragOver(event){
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+
+}
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files;
+    var file = files[0];
+   // processFile(file);
+
+}
+
+function processFile(file){
+    var reader = new FileReader();
+    reader.onload = function(e) {
+
+        $('#userImage').attr('src', e.target.result);
+
+    }
+    reader.readAsDataURL(file);
+
+}
