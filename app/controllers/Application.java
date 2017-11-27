d5ee0a4dfb6d7effbf14dc87e7a4fdea248403ad
adrivarona <adrivarona@gmail.com>
2017-11-27 18:56:53 +0100

    Ready to pull

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,21 @@
 package controllers;
 
 import play.*;
+import play.data.*;
 import play.mvc.*;
 
 import views.html.*;
 
+import models.*;
 public class Application extends Controller {
 
+    Form<Book> bookForm = formFactory.form(Book.class);
 
     public Result index() {
         return ok(index.render());
 
     }
+
+
 
 }
--- app/models/Book.java
+++ app/models/Book.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.awt.*;
+
 public class Book {
+    public Image coverImage;
+    public int NumberOfPages;
+    public String coverType;
+    public String size;
+    public String PaperType;
+    public String dedication;
 }
